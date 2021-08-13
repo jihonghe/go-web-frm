5a61cc05c2623cf923c6a3d0902f272455252952
jihonghe <1654508502@qq.com>
2021-08-13 09:08:50 +0800

    implement base router

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
-module gweb
+module linglong
 
 go 1.16
+
+require (
+	core v0.0.0
+)
+
+replace (
+	core => ./core
+)
--- main.go
+++ main.go
@@ -1,2 +1,25 @@
 package main
 
+import (
+	"core"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// 活下去，寻找希望
+
+func main() {
+	router := core.New()
+
+	router.GET("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	})
+	router.GET("/hello", func(w http.ResponseWriter, r *http.Request) {
+		for k, v := range r.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+
+	log.Fatal(router.Run("0.0.0.0:9999"))
+}
